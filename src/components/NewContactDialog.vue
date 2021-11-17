edd379840a343948a0baf0430e38c6c0e0580cf0
Bakhtiyar Issakhmetov <random997@gmail.com>
2021-11-17 20:10:25 +0300

    Adding saving docs

--- src/components/NewContactDialog.vue
+++ src/components/NewContactDialog.vue
@@ -1,18 +1,54 @@
 <script>
+import database from '@/plugins/firebase/database'
+import {doc, setDoc} from 'firebase/firestore'
+import { encode } from 'js-base64'
+
 export default {
   props: ['open'],
+  data: () => ({
+    loading: false,
+    form: {
+      name: '',
+      phone: ''
+    }
+  }),
+  methods: {
+    create() {
+      const id = this.$store.state.auth.user.uid
+
+      if (!id) {
+        return
+      }
+
+      this.loading = true
+
+      const ref = doc(database, `users/${id}/contacts`, encode(this.form.name + Date.now()))
+      setDoc(ref, this.form)
+          .then(() => {
+            this.$emit('close')
+          })
+          .finally(() => this.loading = false)
+
+    }
+  }
 }
 </script>
 
 <template>
   <v-dialog :value="open" @click:outside="$emit('close')">
-    <v-card>
+    <v-card :loading="loading">
       <v-card-title>
         Новый контакт
       </v-card-title>
 
       <v-card-text>
-        Тут будет форма
+
+        <v-text-field v-model="form.name" label="Имя"/>
+        <v-text-field v-model="form.phone" label="Телефон"/>
+
+        <v-btn :disabled="loading" @click="create">
+          Добавить контакт
+        </v-btn>
       </v-card-text>
 
     </v-card>
